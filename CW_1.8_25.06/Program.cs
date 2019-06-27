6af60c12bfea52c61170b6d9c98ec25cb91bcb49
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-06-27 17:27:00 +0600

    Добавьте файлы проекта.

---
+++ CW_1.8_25.06/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 8.	Вычислить значение логического выражения при всех возможных значениях логических величин X и Y:
+a.	не X и не Y;
+b.	X или (не X и Y);
+c.	(не X и Y) или Y.
+
+     */
+
+namespace CW_1._8_25._06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x = false;
+            bool y = false;
+            do
+            {
+                do
+                {
+                    Console.Write("X="); Console.Write(x); Console.Write(";  Y="); Console.Write(y); Console.WriteLine(";");
+                    Console.Write("а. не X и не Y      = "); Console.WriteLine(!x && !y);
+                    Console.Write("b. X или (не Х и Y) = "); Console.WriteLine(x || (!x && y));
+                    Console.Write("c. (не X и Y) или Y = "); Console.WriteLine((!x && y) || y);
+
+                    y = !y;
+
+                } while (y != false);
+
+                x = !x;
+
+            } while (x != false);
+            
+            Console.ReadKey();
+        }
+    }
+}
